771530f4e1b566186c7385f8127ee137e419ed50
Nir Funt <Antiflag77@gmail.com>
2021-12-11 16:01:19 +0200

    frontend bulid public for heroku

---
+++ public/precache-manifest.01189eb69bad655e0b25470c54e0f111.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad2d94f2e74cc8dd08c",
+    "url": "/css/app.42b610a2.css"
+  },
+  {
+    "revision": "da6f215b4cb4c69834928c7e26bdb0c4",
+    "url": "/fonts/OleoScript-Regular.da6f215b.ttf"
+  },
+  {
+    "revision": "33fc0f73411689c98d99c164d94fe12b",
+    "url": "/img/box.33fc0f73.png"
+  },
+  {
+    "revision": "a6cd4c512234b8b546bf806d19cbba5c",
+    "url": "/img/loader.a6cd4c51.gif"
+  },
+  {
+    "revision": "6daea18ea87451c6913727c4258dadef",
+    "url": "/img/logo/patefon.png"
+  },
+  {
+    "revision": "4a1bfc2bea1819e454c6c329cf386a40",
+    "url": "/img/time.4a1bfc2b.svg"
+  },
+  {
+    "revision": "427e1a0648c2e52b0be776ffe3231956",
+    "url": "/img/upload.427e1a06.svg"
+  },
+  {
+    "revision": "50006f421a5ee5648833d4d578de3cc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ad2d94f2e74cc8dd08c",
+    "url": "/js/app.c32dd1c6.js"
+  },
+  {
+    "revision": "c4d2873006f0e9881961",
+    "url": "/js/chunk-vendors.e3847d01.js"
+  },
+  {
+    "revision": "f9e5b3657a054ba613c3793788bea67a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
